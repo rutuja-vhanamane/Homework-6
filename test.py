2cb0f91146cece1a555216449c6af1ce16be58d1
rutuja-vhanamane <132432933+rutuja-vhanamane@users.noreply.github.com>
2025-06-25 10:38:13 +0530

    Update test.py

--- test.py
+++ test.py
@@ -9,3 +9,14 @@ subjects={
 student1 = students("Rutuja", 101,subjects )
 student1.calculate_percentage()
 student1.display()
+
+
+#OUTPUT
+Name: Rutuja
+Roll no: 101
+Subjects and marks: Math 90
+Subjects and marks: Science 85
+Subjects and marks: English 88
+Subjects and marks: History 91
+Subjects and marks: Computer 95
+Percentage: 89.8
